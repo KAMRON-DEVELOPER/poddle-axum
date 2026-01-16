bfc74bf2c787d6e598608843b30957a0321a3930
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2026-01-16 13:45:41 +0500

    minor fixes

--- tenants/notes/src/app.ts
+++ tenants/notes/src/app.ts
@@ -1,9 +1,9 @@
-import express, { Application, Request, Response } from 'express';
+import express, { type Application, type Request, type Response } from 'express';
 
 const app: Application = express();
 const PORT: number = 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send('Hello World with TypeScript and Express!');
 });
 
