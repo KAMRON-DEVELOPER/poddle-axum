6839ff156e558d2d668466b0ff1102ba86425095
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2026-01-28 17:53:35 +0500

    minor fixes

--- rust/crates/http-contracts/src/pagination/schema.rs
+++ rust/crates/http-contracts/src/pagination/schema.rs
@@ -1,10 +1,14 @@
 use serde::{Deserialize, Serialize};
+use serde_with::{DisplayFromStr, serde_as};
 
+#[serde_as]
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Pagination {
     #[serde(default = "default_offset")]
+    #[serde_as(as = "DisplayFromStr")]
     pub offset: i64,
     #[serde(default = "default_limit")]
+    #[serde_as(as = "DisplayFromStr")]
     pub limit: i64,
 }
 
