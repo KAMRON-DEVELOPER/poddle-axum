326b5d2dac8a78e75fb255b202e4727a73620f5e
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2026-03-04 21:36:39 +0500

    minor fixes

--- rust/crates/http-contracts/src/list/schema.rs
+++ rust/crates/http-contracts/src/list/schema.rs
@@ -1,6 +1,7 @@
+use schemars::JsonSchema;
 use serde::Serialize;
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, JsonSchema, Debug)]
 pub struct ListResponse<T> {
     pub data: Vec<T>,
     pub total: i64,
