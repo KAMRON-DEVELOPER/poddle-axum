cbd0b030a97f42c0c70fcd77f63ce3b53a706fe0
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2026-03-05 13:03:20 +0500

    minor fixes

--- rust/services/compute-api/src/features/webhook.rs
+++ rust/services/compute-api/src/features/webhook.rs
@@ -1,10 +1,11 @@
-use axum::{Json, response::IntoResponse};
+use aide::axum::IntoApiResponse;
+use axum::Json;
 use axum_extra::extract::PrivateCookieJar;
 use serde_json::json;
 
 use crate::error::AppError;
 
 #[allow(unused)]
-pub async fn github_webhook(jar: PrivateCookieJar) -> Result<impl IntoResponse, AppError> {
+pub async fn github_webhook(jar: PrivateCookieJar) -> Result<impl IntoApiResponse, AppError> {
     return Ok(Json(json!({"ok": true})));
 }
